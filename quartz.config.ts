88e2d9c60376dca9f03c0ebdb6ba227520c6f457
Askbatguy <askbatguy@gmail.com>
2026-03-04 10:46:02 +0530

    Quartz sync: Mar 4, 2026, 10:46 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,8 +23,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
+        header: "Bitcount Prop Double",
+        body: "Montserrat",
         code: "IBM Plex Mono",
       },
       colors: {
--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,8 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      "Business Card": "https://askbatguy.carrd.co/",
+      "Shitpost long form :D": "https://www.youtube.com/@Batguyblogs",
     },
   }),
 }
